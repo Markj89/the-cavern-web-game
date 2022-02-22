f4e65c50398d6fbaac89f2a9e58a0e6973736003
Marcus Jackson <marcusjackson@Marcuss-MacBook-Pro.local>
2022-02-22 10:10:26 -0600

    Config Changes #4

--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,15 @@
 // vue.config.js
-module.exports = {}
+module.exports = {
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            fileName: 'index.html',
+            title: 'The Cavern',
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        }
+    },
+    devServer: {
+        open: true
+    }
+}
